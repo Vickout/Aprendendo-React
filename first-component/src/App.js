001a9692f4550db9d3b2123701f9cf1f0e6afc2d
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-21 22:17:33 -0300

    First component

---
+++ first-component/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './Hello';
+import Paragraph from './components/loremIpsum';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <Paragraph />
+    </div>
+  );
+}
+
+export default App;
---
+++ first-component/src/components/loremIpsum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Paragraph() {
+    return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore natus sint itaque omnis 
+        exercitationem. Dolorem velit ducimus accusamus impedit iure? Adipisci quas eligendi, libero 
+        accusamus fugit architecto natus aliquam error?</p>
+}
