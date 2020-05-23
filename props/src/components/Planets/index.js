c1b6cb0824afa2cb0cbb9ffe8683e905b2ff3633
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-23 16:00:59 -0300

    Exercício props

--- props/src/components/Planets/index.js
+++ props/src/components/Planets/index.js
@@ -12,6 +12,7 @@ const Planets = () => {
                 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em 
                 relação ao plano da órbita dentre todos os planetas do Sistema Solar."
                 img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
+                link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
             />
             <Planet 
                 name="Plutão"
@@ -20,6 +21,7 @@ const Planets = () => {
                 como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper, uma região de 
                 corpos além da órbita de Netuno."
                 img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
+                link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
             />
         </Fragment>
     )
--- props/src/components/Planets/planet/index.js
+++ props/src/components/Planets/planet/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import GrayImg from '../../shared/gray_Img'
+import DescriptionWithLink from '../../shared/DescriptionWithLink'
 
 const Planet = (props) => {
     return (
         <div>
             <h4>{props.name}</h4>
             <GrayImg img_url={props.img_url}/>
-            <p>{props.description}</p>
+            <DescriptionWithLink 
+                description={props.description} 
+                link={props.link}
+            />
         </div>
     )
 }
