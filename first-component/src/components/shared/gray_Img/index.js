02b62ddafea1860bb5cbce036b523aa4a2c03af2
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-22 22:07:28 -0300

    Reusing components

---
+++ first-component/src/components/shared/gray_Img/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css"
+
+const GrayImg = () => {
+    return (
+        <img class ="gray-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="mercúrio"/>
+
+    )
+}
+
+export default GrayImg
