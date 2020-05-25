3700f167659ec2d51c9b0674d5ca4c72dae27a12
Victor Lourenco <vlourenco.m@gmail.com>
2020-05-25 15:51:08 -0300

    Controlling multiple inputs

--- controlled-components/src/components/shared/gray_Img/index.js
+++ controlled-components/src/components/shared/gray_Img/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 const GrayImg = (props) => {
     return (
-        <img className={props.gray ? 'gray-img' : 'color-img'} src={props.img_url} />
+        <img className={props.gray ? 'gray-img' : 'color-img'} src={props.img_url} alt=""/>
 
     )
 }
